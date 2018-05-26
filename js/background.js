5291ef739b7c652ad9a3ad6b3ef1cab329de73ed
chaehni <chaehni@student.ethz.ch>
2018-05-27 00:38:48 +0200

    save engine selection

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 const google_url = "http://www.google.com/searchbyimage?image_url=";
 const bing_url = "https://www.bing.com/images/searchbyimage?cbir=ssbi&pageurl=http%3A%2F%2Fwww.squobble.com&imgurl=";
-var engine = 'google'
+let engine = browser.storage.local.get("engine", function (data) {
+    return data["engine"];
+});
 
 browser.contextMenus.create({
     id: "reverse-search",
@@ -8,34 +10,24 @@ browser.contextMenus.create({
     contexts: ["image"]
 });
 
-browser.contextMenus.create({
-    id: "image-url",
-    title: "Image URL",
-    contexts: ["image"]
-});
-
-browser.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == "reverse-search") {
-        url = ""
-        switch (engine){
-            case "google":
-                url = google_url + info.srcUrl;
-                break;
-            case "bing":
-                url = bing_url + info.srcUrl;
-                break;
-            default:
-                url = google_url + info.srcUrl; 
-        }
-        browser.tabs.create({
-            url: url
-        });
-
-    } else if (info.menuItemId == "image-url") {
-        alert(info.srcUrl);
+browser.contextMenus.onClicked.addListener(function (info, tab) {
+    url = ""
+    switch (engine) {
+        case "google":
+            url = google_url + info.srcUrl;
+            break;
+        case "bing":
+            url = bing_url + info.srcUrl;
+            break;
+        default:
+            url = google_url + info.srcUrl;
     }
+    browser.tabs.create({
+        url: url
+    });
 });
 
-function set_engine (eng){
+function set_engine(eng) {
     engine = eng;
+    browser.storage.local.set({ engine: eng });
 }
--- js/popup.js
+++ js/popup.js
@@ -2,10 +2,18 @@ let google = document.getElementById('google');
 let bing = document.getElementById('bing');
 let bg = browser.extension.getBackgroundPage();
 
-google.onclick = function() {
+let engine = browser.storage.local.get("engine", function (data) {
+    if (!data["engine"]) { // Check for engine is undefined
+        google.checked = true;
+    } else {
+        document.getElementById(data["engine"]).checked = true;
+    }
+});
+
+google.onclick = function () {
     bg.set_engine("google");
 };
 
-bing.onclick = function() {
+bing.onclick = function () {
     bg.set_engine("bing");
 };
